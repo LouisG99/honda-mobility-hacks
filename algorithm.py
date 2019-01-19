8c366da8186f805b1eb2a25a9dc3488f4b4f20aa
LouisG99 <louisgrd@umich.edu>
2019-01-19 09:37:58 -0500

    merging Louis

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+# danger lvl (1, 2, 3), dist from car (absolute), 
+# speed, GPS location (calculate with that of radarCar and rel position)
+# 
+
+import pandas as pd 
+import numpy as np
+import json
+
+# other file
+from radarCar import *
+
+
+csvfile = pd.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
+
+df = pd.DataFrame(csvfile)
+driveID = df['driveid'].values
+
+
+start_time = 0.0
+dt = 1e-6 # in s
+
+radarCar = radarCar(start_time, dt)
+
+# load obj distances and angles into object
+radarCar.load_car_Dist(df)
+radarCar.load_car_Angle(df)
+
+logtimes = df['logtime'].values
+
 #Use dataset 1, by calculating speed of vehicles around and all of that
 #Problem with dataset 2 is that vechicles might not be close by, 
 #so might not face same conditions ... 
@@ -10,48 +40,34 @@ import math
 
 class closeCar:
     def __init__(self, distance, angle):
-        self.distance = []
+        self.distance = distance
         self.angle = angle
         
 
 class radarCar:
-    def __init__(self, speed, time, dt):
+    def __init__(self, speed, time):
         self.speed = speed
         self.time = time
-        self.dt = dt
-        self.nearCarsDist = [] # size 63
-        self.nearCarsAngle = [] # size 63
-        
-    def getSpeed(self, dist1, dist2, angle1, angle2):
+        self.nearbyCars = []
+    
+    
+    def getSpeed(self, dist1, dist2, angle1, angle2, dt):
         horiz_d = math.cos(angle1)*dist1 - math.cos(angle2)*dist2
-        vertical_d = math.sin(angle1)*dist1 - math.sin(angle2)*dist2
-        vertical_d += self.speed * self.dt
+        vertical_d = math.sin(angle1)*dist1 - math.sins(angle2)*dist2
+        vertical_d += self.speed*dt
         
         total_d = (horiz_d**2 + vertical_d**2)**0.5
-        return total_d / self.dt
+        return total_d / dt
 
-    def getAverageSpeed(self):
+    def getAverageSpeed(distances, angles, dt):
         speedSum = 0
         count = 0
-        for i in range(len(self.nearCarsAngle)-1):
-            dist_0 = self.nearCarsDist[i]
-            dist_1 = self.nearCarsDist[i+1]
-            if dist_0==0 or dist_1==0: 
-                break
-            else:
-                angle_0 = self.nearCarsAngle[i]
-                angle_1 = self.nearCarsAngle[i+1]
-                speedSum += self.getSpeed(dist_0, dist_1, angle_0, angle_1)
-                count += 1
-        
-        return speedSum / count
-    
-    def getStdevSpeed(self):
-        
+        for car in self.nearByCars:
+            if (distances[count] != 0): 
                 
 
 
 
 import pandas
 
-#csvfile = pandas.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
+csvfile = pandas.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
