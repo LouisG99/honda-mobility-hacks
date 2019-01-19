ece175de4fd13f1c6f4036c7727726d9df80ba35
LouisG99 <louisgrd@umich.edu>
2019-01-19 09:39:28 -0500

    merging Louis

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # danger lvl (1, 2, 3), dist from car (absolute), 
 # speed, GPS location (calculate with that of radarCar and rel position)
 # 
@@ -28,46 +27,3 @@ radarCar.load_car_Angle(df)
 
 logtimes = df['logtime'].values
 
-#Use dataset 1, by calculating speed of vehicles around and all of that
-#Problem with dataset 2 is that vechicles might not be close by, 
-#so might not face same conditions ... 
-#no one might be driving dangerously
-
-# dangerous one at 5:58 in video 
-
-import json
-import math
-
-class closeCar:
-    def __init__(self, distance, angle):
-        self.distance = distance
-        self.angle = angle
-        
-
-class radarCar:
-    def __init__(self, speed, time):
-        self.speed = speed
-        self.time = time
-        self.nearbyCars = []
-    
-    
-    def getSpeed(self, dist1, dist2, angle1, angle2, dt):
-        horiz_d = math.cos(angle1)*dist1 - math.cos(angle2)*dist2
-        vertical_d = math.sin(angle1)*dist1 - math.sins(angle2)*dist2
-        vertical_d += self.speed*dt
-        
-        total_d = (horiz_d**2 + vertical_d**2)**0.5
-        return total_d / dt
-
-    def getAverageSpeed(distances, angles, dt):
-        speedSum = 0
-        count = 0
-        for car in self.nearByCars:
-            if (distances[count] != 0): 
-                
-
-
-
-import pandas
-
-csvfile = pandas.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
