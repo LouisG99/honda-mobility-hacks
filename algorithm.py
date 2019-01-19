d43dc5e81d6ebf828c761f6b112ee3fa91f76f95
LouisG99 <louisgrd@umich.edu>
2019-01-19 02:16:02 -0500

    push

--- algorithm.py
+++ algorithm.py
@@ -10,34 +10,48 @@ import math
 
 class closeCar:
     def __init__(self, distance, angle):
-        self.distance = distance
+        self.distance = []
         self.angle = angle
         
 
 class radarCar:
-    def __init__(self, speed, time):
+    def __init__(self, speed, time, dt):
         self.speed = speed
         self.time = time
-        self.nearbyCars = []
-    
-    
-    def getSpeed(self, dist1, dist2, angle1, angle2, dt):
+        self.dt = dt
+        self.nearCarsDist = [] # size 63
+        self.nearCarsAngle = [] # size 63
+        
+    def getSpeed(self, dist1, dist2, angle1, angle2):
         horiz_d = math.cos(angle1)*dist1 - math.cos(angle2)*dist2
-        vertical_d = math.sin(angle1)*dist1 - math.sins(angle2)*dist2
-        vertical_d += self.speed*dt
+        vertical_d = math.sin(angle1)*dist1 - math.sin(angle2)*dist2
+        vertical_d += self.speed * self.dt
         
         total_d = (horiz_d**2 + vertical_d**2)**0.5
-        return total_d / dt
+        return total_d / self.dt
 
-    def getAverageSpeed(distances, angles, dt):
+    def getAverageSpeed(self):
         speedSum = 0
         count = 0
-        for car in self.nearByCars:
-            if (distances[count] != 0): 
+        for i in range(len(self.nearCarsAngle)-1):
+            dist_0 = self.nearCarsDist[i]
+            dist_1 = self.nearCarsDist[i+1]
+            if dist_0==0 or dist_1==0: 
+                break
+            else:
+                angle_0 = self.nearCarsAngle[i]
+                angle_1 = self.nearCarsAngle[i+1]
+                speedSum += self.getSpeed(dist_0, dist_1, angle_0, angle_1)
+                count += 1
+        
+        return speedSum / count
+    
+    def getStdevSpeed(self):
+        
                 
 
 
 
 import pandas
 
-csvfile = pandas.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
+#csvfile = pandas.read_csv('../autonomous-data/ordered-data.csv', encoding='utf-8')
