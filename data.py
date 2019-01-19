d08b558d11a0554dbc5355fad2e2aef9c3526504
utlathia <utlathia@umich.edu>
2019-01-18 22:38:30 -0500

    Python data pull with JSON object

---
+++ data.py
@@ -0,0 +1,21 @@
+# Making a Python request
+import requests
+import json
+
+# Sample query for device 2004 and trip 12 
+url = "https://ipacdev.hondaresearch.com:8443/hackathon/hondadsrc/evtwarn?device=2004"
+#url = "https://ipacdev.hondaresearch.com:8443/hackathon/hondadsrc/summary"
+
+headers = {
+    'key' : "AC85FK223FNP90AK72",
+    'cache-control' : "no-cache"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+json_obj = json.loads(response.text)
+
+#print(json.dumps(json_obj, indent=4))
+
+with open('dev_2004_evtwarn.json', 'w') as outfile:
+    json.dump(json_obj, outfile)
